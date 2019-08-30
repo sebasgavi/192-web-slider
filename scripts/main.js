e5863745010b349cfcad7e69519233c94d07b85f
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2019-08-30 08:13:34 -0500

    varias imágenes

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,16 @@
 var button = document.querySelector('button');
 var slider = document.querySelector('.slider');
 var tape = slider.querySelector('.slider__tape');
+var count = 0;
 
 function handleClick(event) {
-    var mov = slider.offsetWidth * -1;
+    count++;
+    if(count > tape.childElementCount - 1){
+        count = 0;
+    }
+    var mov = slider.offsetWidth * -1 * count;
     tape.style.transform = 'translate(' + mov + 'px, 0px)';
-    console.log(mov);
+    console.log(tape.childElementCount);
 }
 
 button.addEventListener('click', handleClick);
