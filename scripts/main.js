8a5b0f572c7ab65ec9e474ce7d94b83b7d84492b
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2019-08-30 08:49:52 -0500

    slider reverse

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,33 @@ var slider = document.querySelector('.slider');
 var tape = slider.querySelector('.slider__tape');
 var count = 0;
 
+var first = tape.querySelector('.slider__img');
+var newLast = document.createElement('img');
+newLast.setAttribute('src', first.getAttribute('src'));
+//newLast.setAttribute('class', first.getAttribute('class'));
+newLast.classList.add('slider__img');
+tape.append(newLast);
+
+function handleLast(){
+    console.log('cambio');
+    tape.classList.add('slider__tape--inactive');
+    tape.style.transform = 'translate(0px, 0px)';
+}
+
 function handleClick(event) {
     count++;
-    if(count > tape.childElementCount - 1){
-        count = 0;
+
+    if(count == 1){
+        tape.classList.remove('slider__tape--inactive');
     }
+
     var mov = slider.offsetWidth * -1 * count;
     tape.style.transform = 'translate(' + mov + 'px, 0px)';
-    console.log(tape.childElementCount);
+    
+    if(count > tape.childElementCount - 2){
+        setTimeout(handleLast, 300);
+        count = 0;
+    }
 }
 
 button.addEventListener('click', handleClick);
